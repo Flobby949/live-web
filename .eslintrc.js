05c867cf0ea6aefa6f78fd04861db670781a71c3
jinchenxing <2541226493@qq.com>
2023-11-30 17:17:06 +0800

    refactor(custom): 项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential', // Vue 3 规则
+    'eslint:recommended',
+    '@vue/typescript/recommended', // TypeScript 规则
+    'prettier'
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    extraFileExtensions: ['.vue']
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-unused-vars': 'warn',
+    'eslint-disable-next-line': 'off',
+    'no-undef': 'off'
+  }
+}
